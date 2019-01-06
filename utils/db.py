d6ad1b3dec4657a6ed61bad4ae9bd1a151ef99a2
huiminwu0 <huiminwu24@gmail.com>
2019-01-05 22:36:09 -0500

    Add starting files

---
+++ utils/db.py
@@ -0,0 +1,84 @@
+#lambBaaas -- Hui Min Wu, Raunak Chowdhury, Anton Danylenko
+#SoftDev1 pd8
+#P02 -- The End
+
+import sqlite3   # enable control of an sqlite database
+
+class DB_Manager:
+    '''
+    HOW TO USE:
+    Every method openDB by connecting to the inputted path of
+    a database file. After performing all operations on the
+    database, the instance of the DB_Manager must save using
+    the save method.
+    The operations/methods can be found below. DB_Manager
+    has been custom fitted to work with
+    P00 -- Da Art of Storytellin'
+    '''
+    def __init__(self, dbfile):
+        '''
+        SET UP TO READ/WRITE TO DB FILES
+        '''
+        self.DB_FILE = dbfile
+        self.db = None
+    #========================HELPER FXNS=======================
+    def openDB(self):
+        '''
+        OPENS DB_FILE AND RETURNS A CURSOR FOR IT
+        '''
+        self.db = sqlite3.connect(self.DB_FILE) # open if file exists, otherwise create
+        return self.db.cursor()
+
+    def tableCreator(self, tableName, col0, col1):
+        '''
+        CREATES A 2 COLUMN TABLE IF tableName NOT TAKEN
+        ALL PARAMS ARE STRINGS
+        '''
+        c = self.openDB()
+        if not self.isInDB(tableName):
+            command = "CREATE TABLE '{0}'({1}, {2});".format(tableName, col0, col1)
+            c.execute(command)
+
+
+    def insertRow(self, tableName, data):
+       '''
+         APPENDS data INTO THE TABLE THAT CORRESPONDS WITH tableName
+         @tableName is the name the table being written to
+         @data is a tuple containing data to be entered
+       '''
+       c = self.openDB()
+       command = "INSERT INTO '{0}' VALUES(?, ?)"
+       c.execute(command.format(tableName), data)
+
+
+    def isInDB(self, tableName):
+        '''
+        RETURNS True IF THE tableName IS IN THE DATABASE
+        RETURNS False OTHERWISE
+        '''
+        c = self.openDB()
+        command = "SELECT * FROM sqlite_master WHERE type = 'table'"
+        c.execute(command)
+        selectedVal = c.fetchall()
+        # list comprehensions -- fetch all tableNames and store in a set
+        tableNames = set([x[1] for x in selectedVal])
+
+        return tableName in tableNames
+
+    def table(self, tableName):
+        '''
+        PRINTS OUT ALL ROWS OF INPUT tableName
+        '''
+        c = self.openDB()
+        command = "SELECT * FROM '{0}'".format(tableName)
+        c.execute(command)
+        return c.fetchall()
+
+
+    def save(self):
+        '''
+        COMMITS CHANGES TO DATABASE AND CLOSES THE FILE
+        '''
+        self.db.commit()
+        self.db.close()
+    #========================HELPER FXNS=======================
