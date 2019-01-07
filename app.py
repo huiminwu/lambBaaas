fb8c04a240485c003fcb535a056e892508a51b36
huiminwu0 <huiminwu24@gmail.com>
2019-01-07 00:51:31 -0500

    Add tables and login stuff

---
+++ app.py
@@ -0,0 +1,88 @@
+import json, urllib, os
+
+from flask import Flask, render_template, flash, request, session, redirect, url_for
+
+from utils import db as lamb
+
+DB_FILE = "data/lambBaaas.db"
+app = Flask(__name__)
+user = None
+app.secret_key = os.urandom(32)
+data = lamb.DB_Manager(DB_FILE)
+
+#_choice = ""
+#search = ""
+data.createUsers()
+data.createTyping()
+data.createVocab()
+
+def setUser(userName):
+    global user
+    user = userName
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if user in session:
+        data = lamb.DB_Manager(DB_FILE)
+        return render_template('homepage.html', user_name = user, loggedin = "True")
+
+
+    return render_template("homepage.html")
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    # instantiates DB_Manager with path to DB_FILE
+    data = lamb.DB_Manager(DB_FILE)
+    # LOGGING IN
+    if request.form["action"] == "Login":
+        username, password = request.form["username_login"], request.form['password_login']
+        if username != "" and password != "" and data.verifyUser(username, password ) :
+            session[username] = password
+            setUser(username)
+            data.save()
+            return redirect(url_for('home'))
+        # user was found in DB but password did not match
+        elif data.findUser(username):
+            flash('Incorrect password!')
+        # user not found in DB at all
+        else:
+            flash('Incorrect username!')
+        data.save()
+        return redirect(url_for("home"))
+
+@app.route('/create_account_action', methods=["POST"])
+def create_account_action():
+    data = lamb.DB_Manager(DB_FILE)
+    username, password, password2 = request.form["username_reg"], request.form['password_reg'], request.form['password_check']
+    if len(username.strip()) != 0 and not data.findUser(username):
+        if len(password.strip()) != 0:
+            # add the account to DB
+            if password != password2:
+                flash('Passwords must match')
+            else:
+                data.registerUser(username, password)
+                data.save()
+                flash('Created account')
+                return redirect(url_for('home'))
+        else:
+            flash('Password needs to have stuff in it')
+    elif len(username) == 0:
+        flash("Username needs to have stuff in it")
+    else:
+        flash("Username already taken!")
+    # TRY TO REGISTER AGAIN
+    return render_template("homepage.html")
+
+@app.route('/logout')
+def logout():
+    session.pop(user, None)
+    setUser(None)
+    return redirect(url_for('home'))
+
+@app.route('/return')
+def ret():
+    return redirect(url_for('home'))
+
+if (__name__ == "__main__"):
+    app.debug = True
+    app.run()
--- utils/db.py
+++ utils/db.py
@@ -29,14 +29,34 @@ class DB_Manager:
         self.db = sqlite3.connect(self.DB_FILE) # open if file exists, otherwise create
         return self.db.cursor()
 
-    def tableCreator(self, tableName, col0, col1):
+    def tableCreator2(self, tableName, col0, col1):
         '''
-        CREATES A 2 COLUMN TABLE IF tableName NOT TAKEN
+           CREATES A 2 COLUMN TABLE IF tableName NOT TAKEN
         ALL PARAMS ARE STRINGS
         '''
         c = self.openDB()
         if not self.isInDB(tableName):
             command = "CREATE TABLE '{0}'({1}, {2});".format(tableName, col0, col1)
+            c.execute(command)
+
+    def tableCreator3(self, tableName, col0, col1, col2):
+        '''
+           CREATES A 3 COLUMN TABLE IF tableName NOT TAKEN
+        ALL PARAMS ARE STRINGS
+        '''
+        c = self.openDB()
+        if not self.isInDB(tableName):
+            command = "CREATE TABLE '{0}'({1}, {2}, {3});".format(tableName, col0, col1, col2)
+            c.execute(command)
+
+    def tableCreator4(self, tableName, col0, col1, col2, col3):
+        '''
+           CREATES A 4 COLUMN TABLE IF tableName NOT TAKEN
+        ALL PARAMS ARE STRINGS
+        '''
+        c = self.openDB()
+        if not self.isInDB(tableName):
+            command = "CREATE TABLE '{0}'({1}, {2}, {3}, {4});".format(tableName, col0, col1, col2, col3)
             c.execute(command)
 
 
@@ -82,3 +102,88 @@ class DB_Manager:
         self.db.commit()
         self.db.close()
     #========================HELPER FXNS=======================
+    #===========================DB FXNS========================
+
+    def createUsers(self):
+        '''
+        CREATES TABLE OF users
+        '''
+        self.tableCreator2('users', 'user_name text', 'passwords text')
+        return True
+
+    def createTyping(self):
+        '''
+        CREATES TABLE OF USERS, WPM, TIMESTAMP, AND DIFFICULTY
+        '''
+        self.tableCreator4('typing', 'user_name text', 'wpm int', 'timestamp int', 'difficulty int')
+        return True
+
+    def createVocab(self):
+        '''
+        CREATES TABLE OF VOCAB WORDS AND DEFINITIONS
+        '''
+        self.tableCreator3('vocab', 'user_name text', 'word text', 'definition text')
+        return True
+    def getUsers(self):
+        '''
+        RETURNS A DICTIONARY CONTAINING ALL CURRENT users AND CORRESPONDING PASSWORDS'
+        '''
+        c = self.openDB()
+        command = "SELECT user_name, passwords FROM users"
+        c.execute(command)
+        selectedVal = c.fetchall()
+        return dict(selectedVal)
+
+    def get_user_ids(self, storyTitle):
+        '''
+        RETURNS SET OF user_ids CONTRIBUTED TO storyTitle
+        '''
+        c = self.openDB()
+        command = "SELECT user_id FROM '{0}'".format(storyTitle)
+        c.execute(command)
+        ids = set(x[0] for x in c.fetchall())
+        return ids
+
+    def registerUser(self, userName, password):
+        '''
+        ADDS user TO DATABASE
+        '''
+        c = self.openDB()
+        # userName is already in database -- do not continue to add
+        if self.findUser(userName):
+            return False
+        # userName not in database -- continue to add
+        else:
+            row = (userName, password)
+            self.insertRow('users', row)
+            return True
+
+    def findUser(self, userName):
+        '''
+        CHECKS IF userName IS UNIQUE
+        '''
+        return userName in self.getUsers()
+
+    def verifyUser(self, userName, password):
+        '''
+        CHECKS IF userName AND password MATCH THOSE FOUND IN DATABASE
+        '''
+        c = self.openDB()
+        command = "SELECT user_name, passwords FROM users WHERE user_name = {0}".format("'" + userName + "'")
+        c.execute(command)
+        selectedVal = c.fetchone()
+        if selectedVal == None:
+            return False
+        if userName == selectedVal[0] and password == selectedVal[1]:
+            return True
+        return False
+
+    def getID_fromUser(self, userName):
+        '''
+        RETURNS user_id OF userName
+        '''
+        c = self.openDB()
+        command = "SELECT user_id FROM users WHERE user_name == '{0}'".format(userName)
+        c.execute(command)
+        id = c.fetchone()[0]
+        return id
